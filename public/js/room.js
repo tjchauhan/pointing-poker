50d6d206594f78a0e922214e494df2b581766d31
Tyler Chauhan <tjchauhan7@gmail.com>
2020-03-01 12:33:53 -0500

    Add user list to room

--- public/js/room.js
+++ public/js/room.js
@@ -2,9 +2,11 @@ const socket = io()
 
 // Elements
 const $messages = document.querySelector('#messages')
+const $members = document.querySelector('#members')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const memberTemplate = document.querySelector('#member-template').innerHTML
 
 // Options
 const { roomName, username } = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -13,13 +15,29 @@ socket.on('sendPoints', (message) => {
     console.log(message.points)
 
     const html = Mustache.render(messageTemplate, {
-        points: message.points
+        message: message.points
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('message', (message) => {
-    console.log(message.text)
+socket.on('initialUserList', (message) => {
+    message.users.forEach((user) => {
+        const html = Mustache.render(memberTemplate, {
+            member: user
+        })
+        $members.insertAdjacentHTML('beforeend', html)
+    });
+})
+
+socket.on('updateUserList', (message) => {
+    const html = Mustache.render(memberTemplate, {
+        member: message.username
+    })
+    $members.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('removeUser', (message) => {
+    $members.removeChild(document.getElementById(message.username))
 })
 
 socket.emit('join', { roomName, username })
